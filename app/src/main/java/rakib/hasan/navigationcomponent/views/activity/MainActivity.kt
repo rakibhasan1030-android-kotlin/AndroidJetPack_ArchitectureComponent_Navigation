4b5626d28d5e202391b3eab6c16476d7bd5314d0
Rakib Hasan <rakibhasan1030@gmail.com>
2022-05-17 00:09:58 +0600

    all pages added!

--- app/src/main/java/rakib/hasan/navigationcomponent/views/activity/MainActivity.kt
+++ app/src/main/java/rakib/hasan/navigationcomponent/views/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package rakib.hasan.navigationcomponent
+package rakib.hasan.navigationcomponent.views.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import rakib.hasan.navigationcomponent.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/rakib/hasan/navigationcomponent/views/fragments/ViewBalanceFragment.kt
@@ -0,0 +1,10 @@
+package rakib.hasan.navigationcomponent.views.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import rakib.hasan.navigationcomponent.R
+
+class ViewBalanceFragment : Fragment(R.layout.fragment_view_balance) {}
