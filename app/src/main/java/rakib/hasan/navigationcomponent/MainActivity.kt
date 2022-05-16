b20fb98e003315481c66d9be0131400cf8935b43
Rakib Hasan <rakibhasan1030@gmail.com>
2022-05-16 23:21:10 +0600

    Initial commit

---
+++ app/src/main/java/rakib/hasan/navigationcomponent/MainActivity.kt
@@ -0,0 +1,11 @@
+package rakib.hasan.navigationcomponent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
